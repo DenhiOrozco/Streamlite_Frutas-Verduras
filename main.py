a083125a15096d7e8c8f15266c818b1463b2c411
DenhiOrozco <denhiparati@gmail.com>
2021-05-29 21:09:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,28 @@ import streamlit as st
 siteHeader = st.beta_container()
 with siteHeader:
     st.title('Modelo de reconocimiento de frutas y verduras')
-    st.markdown(""" En este proyecto se busca poder identificar entre frutas y verduras""")
+    st.markdown(""" En este proyecto se busca poder subir una imagen de una fruta o verdura y dejar que el modelo identifique qué es entre las siguientes frutas y verduras:""")
+    
+
+    
+st.markdown(""" <style>
+.main {
+background-color: #AF9EC;
+}
+</style>""",
+unsafe_allow_html=True)
+
+import tensorflow.keras
+from PIL import Image, ImageOps 
+import numpy as np
+
+#Quitar la notación científica para claridad
+np.set_printoptions(suppress=True)
+
+#Cargar el modelo hecho en Teachable Machine con data de Kaggle
+model= tensorflow.keras.models.load_model('/keras_model.h5')
+
+#Crear un array del tamaño correcto para cargar al modelo de keras
+#El largo de las imágenes que se pueden poner en el array está determinado por la primera posición de la tupla de forma.
+
+data= np.ndarray(shape=(1,224,224,3), dtype=np.float32)
